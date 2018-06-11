a9256099eb5f744937d45dd66bf952535679cd3a
gepu0221 <cauc_gp@163.com>
2018-06-11 15:31:44 +0800

    Batch Process

---
+++ BatchDatsetReader.py
@@ -0,0 +1,123 @@
+"""
+Code ideas from https://github.com/Newmu/dcgan and tensorflow mnist dataset reader
+"""
+import numpy as np
+import scipy.misc as misc
+
+
+class BatchDatset:
+    files = []
+    images = []
+    annotations = []
+    image_options = {}
+    batch_offset = 0
+    valid_batch_offset=0
+    epochs_completed = 0
+
+    def __init__(self, records_list, image_options={}):
+        """
+        Intialize a generic file reader with batching for list of files
+        :param records_list: list of file records to read -
+        sample record: {'image': f, 'annotation': annotation_file, 'filename': filename}
+        :param image_options: A dictionary of options for modifying the output image
+        Available options:
+        resize = True/ False
+        resize_size = #size of output image - does bilinear resize
+        color=True/False
+        """
+        print("Initializing Batch Dataset Reader...")
+        print(image_options)
+        self.files = records_list
+        self.image_options = image_options
+        self._read_images()
+
+    def _read_images(self):
+        self.__channels = True
+        self.images = np.array([self._transform(filename['image']) for filename in self.files])
+        self.__channels = False
+        self.annotations =np.expand_dims(np.array(
+            [filename['label'] for filename in self.files]), axis = 1)
+        print (self.images.shape)
+        print (self.annotations.shape)
+        self.filenames=np.array(self.files)
+
+    def _transform(self, filename):
+        #print('image_name',filename)
+        image = misc.imread(filename)
+        if self.__channels and len(image.shape) < 3:  # make sure images are of shape(h,w,3)
+            image = np.array([image for i in range(3)])
+
+        if self.image_options.get("resize", False) and self.image_options["resize"]:
+            resize_size = int(self.image_options["resize_size"])
+            resize_image = misc.imresize(image,
+                                         [resize_size, resize_size], interp='nearest')
+        else:
+            resize_image = image
+
+        return np.array(resize_image)
+
+    def get_records(self):
+        return self.images, self.annotations
+
+    def reset_batch_offset(self, offset=0):
+        self.batch_offset = offset
+
+    def next_batch(self, batch_size):
+        start = self.batch_offset
+        self.batch_offset += batch_size
+        if self.batch_offset > self.images.shape[0]:
+            # Finished epoch
+            self.epochs_completed += 1
+            print("****************** Epochs completed: " + str(self.epochs_completed) + "******************")
+            # Shuffle the data
+            perm = np.arange(self.images.shape[0])
+            np.random.shuffle(perm)
+            self.images = self.images[perm]
+            self.annotations = self.annotations[perm]
+            # Start next epoch
+            start = 0
+            self.batch_offset = batch_size
+
+        end = self.batch_offset
+        return self.images[start:end], self.annotations[start:end]
+    def next_batch_with_name(self,batch_size):
+        start = self.batch_offset
+        self.batch_offset += batch_size
+        if self.batch_offset > self.images.shape[0]:
+            #Finished epoch
+            self.epochs_completed += 1
+            print("****************** Epochs completed: " + str(self.epochs_completed) + "******************")
+            # Shuffle the data
+            perm = np.arange(self.images.shape[0])
+            np.random.shuffle(perm)
+            self.images=self.images[perm]
+            self.annotations = self.annotations[perm]
+            self.filenames = self.filenames[perm]
+            #Start next epoch
+            start = 0
+            self.batch_offset = batch_size
+            
+        end = self.batch_offset
+        return self.images[start:end],self.annotations[start:end],self.filenames[start:end]
+
+    def get_random_batch(self, batch_size):
+        indexes = np.random.randint(0, self.images.shape[0], size=[batch_size]).tolist()
+        return self.images[indexes], self.annotations[indexes],self.filenames[indexes]
+
+    def get_random_batch_for_train(self, batch_size):
+        indexes = np.random.randint(0, self.images.shape[0], size=[batch_size]).tolist()
+        return self.images[indexes], self.annotations[indexes]
+
+    def next_batch_valid(self,batch_size):
+        #print('the number of images',self.images.shape[0])
+        start=self.valid_batch_offset
+        self.valid_batch_offset +=batch_size
+        if_continue=True
+        end=self.valid_batch_offset
+        
+        if self.valid_batch_offset> self.images.shape[0]:
+            if_continue=False
+            end=self.images.shape[0]
+         
+        return self.images[start:end],self.annotations[start:end],self.filenames[start:end], if_continue, start, end
+            
